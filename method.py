286b9b832f8d3d13fca5eb5bfb714fe677da2135
liaohui <1251691629@qq.com>
2019-03-12 20:44:00 +0800

    ddd

---
+++ method.py
@@ -0,0 +1,13 @@
+score = int(input('输入一个数字'))
+if 100 >= score >=90:
+	print('A')
+elif 90 > score >=80:
+	print('B')
+elif 80 > score >=70:
+	print('C')
+elif 70 > score >=60:
+	print('D')
+elif 60 > score:
+	print('E')
+elif score > 100:
+	print('输入错误')
